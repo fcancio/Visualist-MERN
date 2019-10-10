1e2bd73ee7e8ec28e1ae27914301a8265cec60ab
Robin <daorob@gmail.com>
2019-10-10 15:51:24 -0500

    Use checkAuth fun on create.

--- routes/api/gratitude.js
+++ routes/api/gratitude.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gratitudeCtrl = require('../../controllers/gratitude');
-
+const { checkAuth } = require('../utilities');
 
 /* GET /api/gratitude */
 router.get('/:userId', gratitudeCtrl.getCurrentGratitude)
@@ -9,13 +9,6 @@ router.get('/:userId', gratitudeCtrl.getCurrentGratitude)
 
 /* --- Protected Routes --- */
 router.use(require('../../config/auth')); /// this guy does some auth things but not the protecty ones
-router.post('/', gratitudeCtrl.create);
-
-
-/* --- Helper Functions --- */
-// function checkAuth(req, res, next) {
-//     if (req.user) return next();
-//     return res.status(401).json({msg: 'Not Authorized'});
-// }
+router.post('/', checkAuth, gratitudeCtrl.create);
 
 module.exports = router;
