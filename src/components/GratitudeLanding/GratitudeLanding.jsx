5e03bc0e7d02de9c83abae8c56a2130a23b4bbe8
fcancio <46465328+fcancio@users.noreply.github.com>
2019-10-24 11:58:16 -0500

    Update GratitudeLanding.jsx
    remove comments

--- src/components/GratitudeLanding/GratitudeLanding.jsx
+++ src/components/GratitudeLanding/GratitudeLanding.jsx
@@ -4,20 +4,6 @@ import GratitudeList from '../GratitudeList/GratitudeList';
 import styles from '../GratitudeLanding/GratitudeLanding.module.css';
 
 const GratitudeLanding = (props) => {
-    // console.log('gratitude landing props', props)
-
-    // const composedGratitude = props.gratitude.map(item =>{ 
-    //     let createdTime = new Date(item.createdAt).getTime() / 1000 // this will work
-    //     return (
-    //       //TODO: Move <li> block into <GratitudeList/> Component and pass props.gratitude and props.item through
-    //         <GratitudeList
-                
-    //         />
-    //     )
-    // })
-
-               /* //TODO render component <GratitudeList /> */
- 
     console.log('props.gratitude', props.gratitude);
     return (
         props.gratitude.length !== 0 ?
